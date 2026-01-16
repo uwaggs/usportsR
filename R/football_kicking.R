6aac8a7173039ecacd814fbde874bf3d17e5c758
PierreAucoin <159568843+PierreAucoin@users.noreply.github.com>
2026-01-16 15:27:57 -0500

    Enhance documentation for football_kicking function
    Expanded documentation for football_kicking function, detailing return types and column names.

--- R/football_kicking.R
+++ R/football_kicking.R
@@ -2,7 +2,33 @@
 #'
 #' @param year The season(s) of interest.
 #'
-#' @returns A tibble.
+#' @returns
+#' |Column Name     | Type |
+#' |:----------------|:-----|
+#' |player           | chr  |
+#' |punting_no       | int  |
+#' |punting_yds      | int  |
+#' |punting_avg      | dbl  |
+#' |punting_lg       | int  |
+#' |punting_tb       | int  |
+#' |in_20            | int  |
+#' |kicking_fga      | int  |
+#' |kicking_fgm      | int  |
+#' |kicking_lg       | int  |
+#' |kicking_xpa      | int  |
+#' |kicking_xpm      | int  |
+#' |kicking_rg       | int  |
+#' |kicking_pts      | int  |
+#' |kickoffs_no      | int  |
+#' |kickoffs_yds     | int  |
+#' |kickoffs_avg     | dbl  |
+#' |kickoffs_tb      | int  |
+#' |kickoffs_ob      | int  |
+#' |team             | chr  |
+#' |game_id          | chr  |
+#' |season           | chr  |
+#' |season_type      | chr  |
+#'
 #' @examples
 #' football_kicking(2018)
 #' @export
@@ -15,3 +41,4 @@ football_kicking <- function(year = c()) {
   sapply(seasons, function(season) paste0("https://github.com/uwaggs/usports-data/releases/download/football_kicking/kicking_",season,".csv",sep = "")) %>%
     lapply(read_csv) %>% dplyr::bind_rows() %>% tibble::tibble()
 }
+
