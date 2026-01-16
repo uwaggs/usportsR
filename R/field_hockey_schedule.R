8f809f2c3fcdce4969a572938a0f54667e57cca0
PierreAucoin <159568843+PierreAucoin@users.noreply.github.com>
2026-01-16 15:26:08 -0500

    Refactor to get U SPORTS field hockey team box scores
    Updated function to retrieve U SPORTS field hockey team box scores with a parameter for the season. Added detailed documentation for the new function.

--- R/field_hockey_schedule.R
+++ R/field_hockey_schedule.R
@@ -1,10 +1,39 @@
-#' Get U SPORTS field hockey schedule
+#' Get U SPORTS field hockey team box scores
 #'
-#' @returns A tibble.
+#' @param year The season(s) of interest.
+#'
+#' @returns
+#' |Column Name     | Type |
+#' |:----------------|:-----|
+#' |player           | chr  |
+#' |team             | chr  |
+#' |sh               | int  |
+#' |sog              | int  |
+#' |g                | int  |
+#' |a                | int  |
+#' |ds               | int  |
+#' |min              | int  |
+#' |yellow_card      | int  |
+#' |red_card         | int  |
+#' |game_id          | chr  |
+#' |sog_against      | int  |
+#' |ga               | int  |
+#' |sv               | int  |
+#' |goalie_mins      | chr  |
+#' |game_date        | chr  |
+#' |season           | chr  |
+#' |season_type      | chr  |
+#'
 #' @examples
-#' field_hockey_schedule()
+#' field_hockey_team_box_score(2018)
 #' @export
 
-field_hockey_schedule <- function() {
-  read_csv("https://github.com/uwaggs/usports-data/releases/download/field_hockey_schedule/womens_fh_schedule.csv") %>% tibble::tibble()
+field_hockey_team_box_score <- function(year = c()) {
+  if(length(year) < 1){
+    stop("Argument `year` is missing, with no default. Please select a season")
+  }
+  seasons <- create_season(year)
+  sapply(seasons, function(season) paste0("https://github.com/uwaggs/usports-data/releases/download/field_hockey_team_box_score/womens_team_box_score_",season,".csv",sep = "")) %>%
+    lapply(read_csv) %>% dplyr::bind_rows() %>% tibble::tibble()
 }
+
