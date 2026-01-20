09f7897b3f14ef43d583e946e1d26ac4db490814
PierreAucoin <159568843+PierreAucoin@users.noreply.github.com>
2026-01-19 19:28:45 -0500

    Update football_kicking.R

--- R/football_kicking.R
+++ R/football_kicking.R
@@ -3,7 +3,7 @@
 #' @param year The season(s) of interest.
 #'
 #' @returns
-#' |Column Name     | Type |
+#' |Column Name      | Type |
 #' |:----------------|:-----|
 #' |player           | chr  |
 #' |punting_no       | int  |
